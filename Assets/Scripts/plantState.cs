b9f135223a06081112260748f8b1bbd260ff4d18
huidanz18 <robinzheng1018@gmail.com>
2022-03-15 11:07:13 -0400

    plant state

--- Assets/Scripts/plantState.cs
+++ Assets/Scripts/plantState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class landStateTracker : MonoBehaviour
+public class plantState : MonoBehaviour
 {
     // Start is called before the first frame update
     public float watering_interval;
