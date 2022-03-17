10cf0beb4b7fe5147c468d5a66ee94269d8ee665
Cassie Scheirer <cscheire@andrew.cmu.edu>
2022-03-17 12:01:16 -0400

    watering can stuff

--- Assets/WateringCan.cs
+++ Assets/WateringCan.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using XRInteractionToolkit;
 
 public class WateringCan : MonoBehaviour
 {
+    //public XRGrabInteractable can;
+    public Transform rightHand;
+
+    private Transform canOrientation;
+    private Quaternion waterThreshold = Quaternion.Euler(90, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //canOrientation = can.Transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float rotation = Input.GetAxis("Horizontal");
+
+        rotation *= 80 * Time.deltaTime;
+
+        transform.Rotate(0, 0, rotation);
+
+        //check rotation of controller
+        //Transform rightHandRotation = rightHand.Transform.rotation;
+
+        GameObject water = GameObject.Find("water");
         
+        
+
+        //check if rotation meets threshold
+        if (transform.rotation.z >= 0.35f) {
+            water.GetComponent<ParticleSystem>().Play();
+            Debug.Log(transform);
+        }
+        else {
+            water.GetComponent<ParticleSystem>().Stop();
+
+            Debug.Log(transform.rotation.z);
+        }
+
+        //if rotation meets threshold, turn on water particles
+
+        //if rotation stops threshold, turn off water
     }
 }
